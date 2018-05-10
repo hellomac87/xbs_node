ba724037c7c511e57a9c3bb87bafb7372b9b5867
leegangsan <nouvin@certon.co.kr>
2018-05-10 12:16:07 +0900

    passport localStrategy 구현

--- app.js
+++ app.js
@@ -3,6 +3,14 @@ const app = express();
 
 const logger = require('morgan');
 const bodyParser = require('body-parser');
+const cookieParser = require('cookie-parser');
+
+// flash message
+const flash = require('connect-flash');
+
+// passport login
+const passport = require('passport');
+const session = require('express-session');
 
 //mongodb connect
 const mongoose = require('mongoose');
@@ -17,10 +25,6 @@ db.once('open', function(){
 const connect = mongoose.connect('mongodb://127.0.0.1:27017/newsTest',{useMongoClient : true});
 autoIncrement.initialize(connect);
 
-const index = require('./routes/index');
-const admin = require('./routes/admin');
-const regist = require('./routes/regist');
-
 // 미들웨어 세팅
 app.use(logger('dev'));
 app.use(bodyParser.json());
@@ -31,6 +35,31 @@ app.set('view engine', 'ejs');
 
 app.use('/static', express.static('static'));
 app.use('/uploads', express.static('uploads'));
+
+// session setting (passport?)
+app.use(session({
+        secret: 'fastcampus',
+        resave: false,
+        saveUninitialized: true,
+        cookie: {
+            maxAge: 2000 * 60 * 60 //지속시간 2시간
+        }
+    })
+);
+
+//passport 적용
+app.use(passport.initialize());
+app.use(passport.session());
+
+//플래시 메시지 관련
+app.use(flash());
+
+
+
+
+const index = require('./routes/index');
+const admin = require('./routes/admin');
+const regist = require('./routes/regist');
 
 app.use('/', index);
 app.use('/xbs_admin', admin);
--- routes/admin.js
+++ routes/admin.js
@@ -3,21 +3,53 @@ const router = express.Router();
 const passwordHash = require('../libs/passwordHash');
 const UserModel = require('../models/UserModel');
 
-// sign in router
+// passport
+const passport = require('passport');
+const LocalStrategy = require('passport-local').Strategy;
 
+//passport serializeUser, deserializeUser
+passport.serializeUser(function(user, done){
+    console.log('serializeUser');
+    done(null, user);
+});
+passport.deserializeUser(function(user, done){
+    console.log('deserializeUser');
+    done(null, user);
+});
+
+// passport 정책 작성
+passport.use(new LocalStrategy({
+        usernameField:'username',
+        passwordField:'password',
+        session:true, // 세션 저장 여부
+        passReqToCallback:true
+    },
+    (req, username, password, done) => {
+        UserModel.findOne({
+            username:req.body.username,
+            password:passwordHash(req.body.password)
+        },(err,user) => {
+           if(err){ console.log(err) }
+           if(!user){
+               return done(null, false, {message:'아이디 또는 비밀번호 오류'});
+           }else{
+               return done(null, user);
+           }
+        });
+    }
+));
+
+// sign in router
 router.get('/', (req, res) => {
     res.render('login');
 });
-router.post('/', (req, res) => {
-    UserModel.findOne({
-        username:req.body.username,
-        password:passwordHash(req.body.password)
-    }, function(err, user){
-        if(!user){
-            res.send('there is no user');
-        }
-        res.send(user);
-    });
+
+router.post('/', passport.authenticate('local',{
+        failureRedirect:'/xbs_admin', //실패시 리다이렉트
+        failureFlash:true //실패시 플래시 메세지
+    }),
+    (req, res) => {
+        res.send(req.user);
 });
 
 // sign up router
